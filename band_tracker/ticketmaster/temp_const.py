b18d4c9925111a66f2a1fe835f44b4c76b0636ed
egord <egor.dmitriev99@gmail.com>
2023-07-06 15:24:34 +0200

    mooooooove

--- band_tracker/ticketmaster/temp_const.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("./band_tracker/ticketmaster/artists.json", "r") as openfile:
-    ARTISTS = json.load(openfile)
-
-with open("./band_tracker/ticketmaster/events.json", "r") as openfile:
-    EVENTS = json.load(openfile)
