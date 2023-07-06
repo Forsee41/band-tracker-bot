a0bfe265600ec497ba60f8cc2bccabbde3a8d646
Forsee41 <forsee41@gmail.com>
2023-07-06 18:14:49 +0300

    fix test setup fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,10 @@ import os
 
 import pytest
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
-import band_tracker.db.models
 from band_tracker.db.dal import DAL
+from band_tracker.db.models import Base
 from band_tracker.db.session import AsyncSessionmaker
 
 
@@ -27,12 +28,16 @@ def db_creds(load_dotenv_) -> dict:
 
 
 @pytest.fixture(scope="session", autouse=True)
-def create_tables(sessionmaker) -> None:
-    engine = sessionmaker.engine
-    try:
-        band_tracker.db.models.Base.metadata.create_all(engine)
-    except Exception:
-        pass
+def create_tables(db_creds: dict) -> None:
+    login = db_creds["login"]
+    pw = db_creds["password"]
+    ip = db_creds["ip"]
+    port = db_creds["port"]
+    db = db_creds["database"]
+    db_url = f"postgresql://" f"{login}:" f"{pw}@" f"{ip}:" f"{port}/" f"{db}"
+
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
 
 
 @pytest.fixture(scope="session")
@@ -42,6 +47,7 @@ def sessionmaker(db_creds: dict) -> AsyncSessionmaker:
 
 
 @pytest.fixture(scope="class")
-def dal(sessionmaker: AsyncSessionmaker) -> DAL:
+def dal(create_tables, sessionmaker: AsyncSessionmaker) -> DAL:
+    assert create_tables is None
     dal = DAL(sessionmaker)
     return dal
