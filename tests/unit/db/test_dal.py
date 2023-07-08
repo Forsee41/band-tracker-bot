6771695340dd62547e12b2f3edb9318d6394f9fc
Forsee41 <forsee41@gmail.com>
2023-07-08 19:54:46 +0300

    add dal get by artist tm id test

--- tests/unit/db/test_dal.py
+++ tests/unit/db/test_dal.py
@@ -9,17 +9,17 @@ from band_tracker.db.models import ArtistDB
 
 
 class TestDAL:
+    default_artist = Artist(
+        name="name",
+        spotify_link=HttpUrl("https://spotify.com"),
+        tickets_link=HttpUrl("https://tickets_url.com"),
+        inst_link=HttpUrl("https://inst.com"),
+        youtube_link=HttpUrl("https://youtube.com"),
+        source_specific_data={EventSource.ticketmaster_api: {"id": "ticketmaster_id"}},
+    )
+
     async def test_add_artist(self, dal: DAL) -> None:
-        artist = Artist(
-            name="name",
-            spotify_link=HttpUrl("https://spotify.com"),
-            tickets_link=HttpUrl("https://tickets_url.com"),
-            inst_link=HttpUrl("https://inst.com"),
-            youtube_link=HttpUrl("https://youtube.com"),
-            source_specific_data={
-                EventSource.ticketmaster_api: {"id": "ticketmaster_id"}
-            },
-        )
+        artist = self.default_artist
         artist_id = await dal.add_artist(artist)
 
         stmt = select(ArtistDB).where(ArtistDB.id == artist_id)
@@ -32,6 +32,18 @@ class TestDAL:
         assert selected_artist.tickets_link == "https://tickets_url.com/"
         assert selected_artist.inst_link == "https://inst.com/"
         assert selected_artist.youtube_link == "https://youtube.com/"
+
+    async def test_get_artist_by_tm_id(self, dal: DAL) -> None:
+        artist = self.default_artist
+        await dal.add_artist(artist)
+        result_artist = await dal.get_artist_by_tm_id("ticketmaster_id")
+
+        assert result_artist
+        assert result_artist.name == "name"
+        assert result_artist.spotify_link == HttpUrl("https://spotify.com/")
+        assert result_artist.tickets_link == HttpUrl("https://tickets_url.com/")
+        assert result_artist.inst_link == HttpUrl("https://inst.com/")
+        assert result_artist.youtube_link == HttpUrl("https://youtube.com/")
 
 
 if __name__ == "__main__":
