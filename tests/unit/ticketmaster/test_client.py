d994177a5dac44b17cba61595205dc51c3d1d820
egord <egor.dmitriev99@gmail.com>
2023-07-11 15:35:04 +0200

    tests for get_all_{events, artists}

--- tests/unit/ticketmaster/test_client.py
+++ tests/unit/ticketmaster/test_client.py
@@ -6,8 +6,13 @@ from pydantic import HttpUrl
 from band_tracker.core.artist_update import ArtistUpdate, ArtistUpdateSocials
 from band_tracker.core.enums import EventSource
 from band_tracker.core.event_update import EventUpdate, EventUpdateSales
-from band_tracker.ticketmaster.client import get_artist, get_event
-from test_data.temp_const import ARTISTS, EVENTS
+from band_tracker.ticketmaster.client import (
+    get_all_artists,
+    get_all_events,
+    get_artist,
+    get_event,
+)
+from test_data.temp_const import ARTISTS, ARTISTS_SMALL, EVENTS
 
 
 class TestClient:
@@ -225,6 +230,221 @@ class TestClient:
             ),
         )
         assert get_event(raw_event) == event
+
+    def test_get_all_artists(self) -> None:
+        artists_list = [
+            ArtistUpdate(
+                name="Solina Cello-Ensemble",
+                socials=ArtistUpdateSocials(
+                    spotify=None,
+                    instagram=None,
+                    youtube=None,
+                ),
+                aliases=[],
+                tickets_link=HttpUrl(
+                    "https://www.ticketmaster.com/solina-celloensemble-tickets/artist/2772347"
+                ),
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "K8vZ917_Fi7"}
+                },
+                genres=["Classical", "Classical/Vocal"],
+                images=[
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_TABLET_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_RETINA_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_TABLET_LANDSCAPE_LARGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_RETINA_PORTRAIT_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_RETINA_PORTRAIT_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_TABLET_LANDSCAPE_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_EVENT_DETAIL_PAGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_CUSTOM.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_RECOMENDATION_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e-a7aa-1932aab77518_105461_ARTIST_PAGE_3_2.jpg"
+                    ),
+                ],
+            ),
+            ArtistUpdate(
+                name="Maze featuring Frankie Beverly",
+                socials=ArtistUpdateSocials(
+                    spotify=None,
+                    instagram=None,
+                    youtube=None,
+                ),
+                aliases=[],
+                tickets_link=HttpUrl(
+                    "https://www.ticketmaster.com/maze-featuring-frankie-beverly-tickets/artist/735607"
+                ),
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "K8vZ9171Idf"}
+                },
+                genres=["R&B", "Soul"],
+                images=[
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_TABLET_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_TABLET_LANDSCAPE_LARGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_RETINA_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_RETINA_PORTRAIT_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_RECOMENDATION_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_CUSTOM.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_EVENT_DETAIL_PAGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_ARTIST_PAGE_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_RETINA_PORTRAIT_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-4406b8ab64cd_1600911_TABLET_LANDSCAPE_3_2.jpg"
+                    ),
+                ],
+            ),
+        ]
+        assert get_all_artists(ARTISTS_SMALL) == artists_list
+
+    def test_get_all_events(self) -> None:
+        events_list = [
+            EventUpdate(
+                title="Shania Twain: Queen Of Me Tour",
+                date=datetime(2023, 7, 15),
+                venue="Ruoff Music Center",
+                ticket_url=HttpUrl(
+                    "https://concerts.livenation.com/shania-twain-queen-of-me-tour-"
+                    "noblesville-indiana-07-15-2023/event/05005D55DD6D454E"
+                ),
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "vvG1fZ949qhf4C"}
+                },
+                venue_city="Noblesville",
+                venue_country="United States Of America",
+                artists=["K8vZ91719n0", "K8vZ917_bOf"],
+                images=[
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47"
+                        "cc9b10-4904-4dec-b1d6-539e44a521d1_1825531_RETINA_PORTRAIT_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_EVENT_DETAIL_PAGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904"
+                        "-4dec-b1d6-539e44a521d1_1825531_RETINA_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_TABLET_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_RECOMENDATION_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_TABLET_LANDSCAPE_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904"
+                        "-4dec-b1d6-539e44a521d1_1825531_TABLET_LANDSCAPE_LARGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_CUSTOM.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_RETINA_PORTRAIT_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+                        "4dec-b1d6-539e44a521d1_1825531_ARTIST_PAGE_3_2.jpg"
+                    ),
+                ],
+                sales=EventUpdateSales(
+                    on_sale=True, price_max=249.95, price_min=39.95, currency="USD"
+                ),
+            ),
+            EventUpdate(
+                title="Imagine Dragons",
+                date=datetime(2023, 7, 8),
+                venue="American Family Insurance Amphitheater - Summerfest Grounds",
+                ticket_url=HttpUrl(
+                    "https://www.ticketmaster.com/imagine-dragons-milwaukee-wisconsin-07-08-2023/event/07005D689BED1C00"
+                ),
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "vvG1jZ9pxhDoYZ"}
+                },
+                venue_city="Milwaukee",
+                venue_country="United States Of America",
+                artists=["K8vZ917GSz7"],
+                images=[
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_RECOMENDATION_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_ARTIST_PAGE_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_RETINA_PORTRAIT_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_RETINA_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_TABLET_LANDSCAPE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_TABLET_LANDSCAPE_LARGE_16_9.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_TABLET_LANDSCAPE_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_RETINA_PORTRAIT_3_2.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_CUSTOM.jpg"
+                    ),
+                    HttpUrl(
+                        "https://s1.ticketm.net/dam/a/c90/67a18d21-394f-4afe-84f9-15560d797c90_1652911_EVENT_DETAIL_PAGE_16_9.jpg"
+                    ),
+                ],
+                sales=EventUpdateSales(
+                    on_sale=True, price_max=231.38, price_min=87.63, currency="USD"
+                ),
+            ),
+        ]
+        assert get_all_events(ARTISTS_SMALL) == events_list
 
 
 if __name__ == "__main__":
