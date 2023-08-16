e00ff66b64bc416a04274ac1aa39a7af17cda037
Forsee41 <forsee41@gmail.com>
2023-08-16 21:33:24 +0300

    add artist text search tests

--- tests/unit/db/test_artist_search.py
+++ tests/unit/db/test_artist_search.py
@@ -6,30 +6,48 @@ from band_tracker.db.artist_update import ArtistUpdate
 from band_tracker.db.dal import BotDAL, UpdateDAL
 
 
-class TestAddArtistDAL:
-    async def test_add_artist(
-        self, update_dal: UpdateDAL, get_artist_update: Callable[[str], ArtistUpdate]
+class TestArtistTextSearch:
+    @pytest.mark.parametrize(
+        ("search_str", "similarity", "expected_result"),
+        (
+            ["osha", 0.2, ["gosha"]],
+            ["gos", 0.3, ["gosha"]],
+            ["toh", 0.4, ["anton"]],
+            ["clarnet", 0.9, ["clara"]],
+            ["cal", 0.2, ["clara"]],
+            ["a", -0.5, ["gosha", "anton", "clara"]],
+        ),
+    )
+    async def test_basic_search(
+        self,
+        update_dal: UpdateDAL,
+        get_artist_update: Callable[[str], ArtistUpdate],
+        bot_dal: BotDAL,
+        search_str: str,
+        similarity: float,
+        expected_result: list[str],
     ) -> None:
-        artist = get_artist_update("gosha")
-        await update_dal._add_artist(artist)
-        result_artist = await update_dal._get_artist_by_tm_id("gosha_tm_id")
+        artist_names = ["gosha", "clara", "anton"]
+        artist_updates = [get_artist_update(artist) for artist in artist_names]
+        for artist in artist_updates:
+            await update_dal._add_artist(artist)
+        result = await bot_dal.search_artist(search_str, similarity_min=similarity)
+        assert set(result) == set(expected_result)
 
-        assert result_artist
-        assert result_artist.name == "gosha"
-        assert result_artist.tickets_link == "https://gosha_tickets.com/"
-
-    async def test_same_tm_id_fails(
+    async def test_search_similar_aliases(
         self,
         update_dal: UpdateDAL,
         get_artist_update: Callable[[str], ArtistUpdate],
         bot_dal: BotDAL,
     ) -> None:
         artist_names = ["gosha", "clara", "anton"]
-        artist_updates = [get_artist_update(artist) for artist in artist_names]
-        for artist in artist_updates:
+        gosha, clara, anton = [get_artist_update(artist) for artist in artist_names]
+        gosha.aliases.append("goshturbator")
+        anton.aliases.append("anturbator")
+        for artist in gosha, anton, clara:
             await update_dal._add_artist(artist)
-        result = await bot_dal.search_artist("gosa", similarity_min=0.3)
-        assert result == ["gosha"]
+        result = await bot_dal.search_artist("turbator", similarity_min=0.3)
+        assert set(result) == set(["gosha", "anton"])
 
 
 if __name__ == "__main__":
