3bb9af699b11bb504340cb4ab72fdc46c24de6ea
Forsee41 <forsee41@gmail.com>
2023-08-21 22:21:30 +0300

    add bot query handle

--- band_tracker/bot/handlers/test.py
+++ band_tracker/bot/handlers/test.py
@@ -3,17 +3,26 @@ import logging
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
+from band_tracker.db.dal import BotDAL
+
 log = logging.getLogger(__name__)
 
 
-async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def query_artists(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    args = context.args
+    assert args
+    query = args[0]
+    dal: BotDAL = context.bot_data["dal"]
+    result_artists = await dal.search_artist(query)
+    result_artist_names = [artist.name for artist in result_artists]
+    result_str = ", ".join(result_artist_names)
     if update.effective_chat:
-        log.info("Test handler invoked")
         await context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Test handler works!"
+            chat_id=update.effective_chat.id, text=result_str
         )
     else:
         log.warning("Test handler can't find an effective chat of an update")
+    assert update.effective_chat
 
 
-handlers = [CommandHandler("test", test)]
+handlers = [CommandHandler("query", query_artists)]
