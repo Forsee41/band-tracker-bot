9e0bf871a32a108c1b89364bfe0737ce512f2576
Forsee41 <forsee41@gmail.com>
2023-09-12 14:29:06 +0300

    add linear predictor tests

--- band_tracker/updater/timestamp_predictor.py
+++ band_tracker/updater/timestamp_predictor.py
@@ -1,6 +1,7 @@
-import math
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
+
+import sympy
 
 
 class TimestampPredictor(ABC):
@@ -14,24 +15,28 @@ class TimestampPredictor(ABC):
 class LinearPredictor(TimestampPredictor):
     """Using ax+b formula for predictions. The unit is day"""
 
-    def __init__(self, a: float, b: float) -> None:
+    def __init__(self, a: float, b: float, start: datetime | None) -> None:
         self._a = a
         self._b = b
-        self._start = datetime.now()
+        self._start = start if start is not None else datetime.now()
+
+    def _calculate(self, start: int, target_area: int) -> int:
+        a = self._a
+        b = self._b
+        c = target_area
+        x1 = start
+        x2 = sympy.symbols("x2")
+        eq = sympy.Eq(((a * (x1 + x2) + (2 * b)) / 2) * (x2 - x1), (c))
+        solution = sympy.solve(eq, x2)
+        solution = [(int(item)) for item in solution]
+        return min(solution)
 
     def get_next_timestamp(
         self, starting_timestamp: datetime, target_entities: int
     ) -> datetime:
         time_passed_to_start: timedelta = starting_timestamp - self._start
         days_passed_to_start: int = time_passed_to_start.days
-        left_integr = (
-            (days_passed_to_start**2) * self._a * 0.5
-        ) + days_passed_to_start * self._b
-        a_term = self._a * 0.5
-        b_term = self._b
-        c_term = -(left_integr + target_entities)
-        disc = b_term**2 - (4 * a_term * c_term)
-        solution1 = (-b_term - math.sqrt(disc)) / (2 * a_term)
-        solution2 = (-b_term + math.sqrt(disc)) / (2 * a_term)
-        real_solution = max([solution1, solution2])
-        return self._start + timedelta(days=real_solution)
+        target_days_passed = self._calculate(
+            start=days_passed_to_start, target_area=target_entities
+        )
+        return self._start + timedelta(days=int(target_days_passed))
