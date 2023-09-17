1c54df994294bd897104d8ac2becf62ea176edf6
Forsee41 <forsee41@gmail.com>
2023-09-17 15:30:01 +0300

    refactor query handler

--- band_tracker/bot/handlers/query.py
+++ band_tracker/bot/handlers/query.py
@@ -9,18 +9,23 @@ log = logging.getLogger(__name__)
 
 
 async def query_artists(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    dal: BotDAL = context.bot_data["dal"]
+
     args = context.args
     assert args
     query = "".join(args)
+
     if len(query) > 255:
         query = query[:255]
-    dal: BotDAL = context.bot_data["dal"]
+
     result_artists = await dal.search_artist(query)
     result_artist_names = [artist.name for artist in result_artists]
+
     if result_artist_names:
         result_str = ", ".join(result_artist_names)
     else:
         result_str = "No artists found!"
+
     if update.effective_chat:
         await context.bot.send_message(
             chat_id=update.effective_chat.id, text=result_str
