42979cd9343b2eddaf05a8662057f87a3b2fe2ae
Forsee41 <forsee41@gmail.com>
2023-09-18 19:07:16 +0300

    add mq producer and consumer

---
+++ band_tracker/mq_publisher.py
@@ -0,0 +1,53 @@
+import json
+from enum import Enum
+
+from aio_pika import DeliveryMode, Message, connect
+from aio_pika.abc import AbstractConnection
+
+
+class MessageType(Enum):
+    notification = "notification"
+
+
+class MQPublisher:
+    _url: str
+    _key: str
+    _exchange_name: str
+    _connection: AbstractConnection
+
+    @classmethod
+    async def create(cls: type, routing_key: str, url: str, exchange: str) -> None:
+        self = cls()
+        self._key = routing_key
+        self._url = url
+        self._exchange_name = exchange
+        self.connection = await self.connect()
+
+    async def connect(self) -> AbstractConnection:
+        connection = await connect(self._url)
+        return connection
+
+    async def send_message(
+        self,
+        data: dict,
+        type_: MessageType,
+        headers: dict = {},
+        persistent: bool = True,
+    ) -> None:
+        persistence = (
+            DeliveryMode.PERSISTENT if persistent else DeliveryMode.NOT_PERSISTENT
+        )
+        msg_body = json.dumps(data)
+        message = Message(
+            bytes(msg_body, "utf-8"),
+            content_type="application/json",
+            headers=headers,
+            type=type_.value,
+            delivery_mode=persistence,
+        )
+        async with self._connection:
+            channel = await self._connection.channel()
+            exchange = await channel.declare_exchange(
+                self._exchange_name, auto_delete=False
+            )
+            await exchange.publish(message, routing_key=self._key)
--- band_tracker/notifier.py
+++ band_tracker/notifier.py
@@ -1,20 +1,51 @@
-import os
-
 import asyncio
+
+from aio_pika import ExchangeType, connect
+from aio_pika.abc import AbstractConnection, AbstractIncomingMessage
 from telegram import Bot
 
 
 class Notifier:
-    def __init__(self, bot: Bot, admin_chats: list[int]) -> None:
+    bot: Bot
+    admin_chats: list[int]
+    mq_routing_key: str
+    mq_connection: AbstractConnection
+    mq_exchange_name: str
+
+    @classmethod
+    async def create(
+        cls: type,
+        bot: Bot,
+        admin_chats: list[int],
+        mq_url: str,
+        mq_routing_key: str,
+        exchange_name: str,
+    ) -> None:
+        self = cls()
         self.bot = bot
         self.admin_chats = admin_chats
+        self.mq_url = mq_url
+        self.mq_routing_key = mq_routing_key
+        self.mq_exchange_name = exchange_name
+        self.mq_connection = await connect(mq_url)
+
+    async def consume(self) -> None:
+        connection = self.mq_connection
+        async with connection:
+            channel = await connection.channel()
+
+            exchange = await channel.declare_exchange(
+                self.mq_exchange_name, ExchangeType.DIRECT
+            )
+            queue = await channel.declare_queue("notifier_queue")
+            await queue.bind(exchange, self.mq_routing_key)
+            await queue.consume(self.on_message)
+            await asyncio.Future()
 
     async def notify_admins(self, text: str) -> None:
         for admin_chat_id in self.admin_chats:
             await self.bot.sendMessage(chat_id=admin_chat_id, text=text)
 
-
-def main() -> None:
-    token = os.getenv("TELEGRAM_BOT_TOKEN", "")
-    bot = Bot(token=token)
-    asyncio.run(bot.sendMessage(chat_id=986579738, text="Hi!!!!!!!!!"))
+    async def on_message(self, message: AbstractIncomingMessage) -> None:
+        async with message.process():
+            print(f"Received message: {message.body.decode()}")
