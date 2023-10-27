f767461853080ef1dbd71ce18d289cdca9d84e96
Forsee41 <forsee41@gmail.com>
2023-10-27 04:06:13 +0300

    add inline artist query handler

---
+++ band_tracker/bot/handlers/find_inline_query.py
@@ -0,0 +1,35 @@
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import ContextTypes, InlineQueryHandler
+
+from band_tracker.db.dal import BotDAL
+
+
+async def handle_inline_query(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    dal: BotDAL = context.bot_data["dal"]
+
+    if not update.inline_query:
+        return
+
+    query_raw = update.inline_query.query
+    query = " ".join(query_raw.split()[1:])
+    result_artists = await dal.search_artist(query)
+    inline_results: list[InlineQueryResultArticle] = []
+    id = 0
+    for artist in result_artists:
+        thumbnail = artist.image if artist.image else "https://i.imgur.com/u8XHujc.jpeg"
+        query_result = InlineQueryResultArticle(
+            id=str(id),
+            title=artist.name,
+            description=str(artist.genres if artist.genres else "No genres"),
+            thumbnail_url=thumbnail,
+            input_message_content=InputTextMessageContent(artist.name),
+        )
+        inline_results.append(query_result)
+        id += 1
+    await update.inline_query.answer(inline_results)
+
+
+pattern = "^/find*"
+handlers = [InlineQueryHandler(callback=handle_inline_query)]
--- band_tracker/bot/handlers_registrator.py
+++ band_tracker/bot/handlers_registrator.py
@@ -1,13 +1,10 @@
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, BaseHandler
 
-from band_tracker.bot.handlers import query, test
+from band_tracker.bot.handlers import find_inline_query, query, test
 
 
-def _get_handlers() -> list[CommandHandler]:
-    handler_lists = [
-        query.handlers,
-        test.handlers,
-    ]
+def _get_handlers() -> list[BaseHandler]:
+    handler_lists = [query.handlers, test.handlers, find_inline_query.handlers]
 
     result = []
     for handler_list in handler_lists:
