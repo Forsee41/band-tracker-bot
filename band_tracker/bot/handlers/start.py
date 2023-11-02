753208a9b44be6cd738ce08df7019a0a89ca829d
Forsee41 <forsee41@gmail.com>
2023-11-02 11:55:59 +0300

    add bot user helpers

--- band_tracker/bot/handlers/start.py
+++ band_tracker/bot/handlers/start.py
@@ -1,11 +1,9 @@
 import logging
-from datetime import datetime
 
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
-from band_tracker.core.user import User
-from band_tracker.core.user_settings import UserSettings
+from band_tracker.bot.user_helper import get_user
 from band_tracker.db.dal_bot import BotDAL
 
 log = logging.getLogger(__name__)
@@ -20,19 +18,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         log.warning("Start handler can't find an effective user!")
         return
 
-    user_settings = UserSettings.default()
-    user = User(
-        id=int(user_tg.id),
-        name=user_tg.name,
-        subscriptions=[],
-        follows=[],
-        join_date=datetime.now(),
-        settings=user_settings,
-    )
+    user = await get_user(tg_user=user_tg, dal=dal)
 
     if not update.effective_chat:
         log.warning("Start handler can't find an effective chat of an update")
         return
+
     welcoming_text = f"Welcome {user.name}! Use `/help` command to get started!"
     await dal.add_user(user)
     await context.bot.send_message(
---
+++ band_tracker/bot/user_helper.py
@@ -0,0 +1,36 @@
+import logging
+from datetime import datetime
+
+from telegram import User as TgUser
+
+from band_tracker.core.user import User
+from band_tracker.core.user_settings import UserSettings
+from band_tracker.db.dal_bot import BotDAL
+
+log = logging.getLogger(__name__)
+
+
+def default_user(user_tg: TgUser) -> User:
+    user_settings = UserSettings.default()
+    user = User(
+        id=int(user_tg.id),
+        name=user_tg.name,
+        subscriptions=[],
+        follows=[],
+        join_date=datetime.now(),
+        settings=user_settings,
+    )
+    return user
+
+
+async def get_user(tg_user: TgUser, dal: BotDAL) -> User:
+    """
+    Returns core User for a passed tg user, or registers a new one
+    and returns it if user doesn't exist.
+    """
+    user = await dal.get_user(tg_user.id)
+    if user:
+        return user
+    user = default_user(tg_user)
+    await dal.add_user(user)
+    return user
