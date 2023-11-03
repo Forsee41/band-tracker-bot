01aa445994f87296133121c5496744b7ea8ca872
Forsee41 <forsee41@gmail.com>
2023-11-03 13:52:49 +0300

    add artist socials and genres to amp

--- band_tracker/bot/handlers/artist.py
+++ band_tracker/bot/handlers/artist.py
@@ -84,11 +84,24 @@ async def _show_followed_amp(bot: Bot, chat_id: int, artist: Artist) -> None:
 async def _send_result(
     bot: Bot, chat_id: int, artist: Artist, markup: InlineKeyboardMarkup
 ) -> None:
+    text_data = f"<b>{artist.name}</b>\n"
+    if artist.genres:
+        genres = " ".join(artist.genres)
+        genres_str = f"Genres: {genres}\n"
+        text_data += genres_str
+    if artist.socials.instagram:
+        text_data += f"[Instagram]({artist.socials.instagram})\n"
+    if artist.socials.youtube:
+        text_data += f"[Youtube]({artist.socials.youtube})\n"
+    if artist.socials.spotify:
+        text_data += f"[Spotify]({artist.socials.spotify})\n"
+
     await bot.send_photo(
         chat_id=chat_id,
         photo=artist.image,  # type: ignore
         caption="Text data",
         reply_markup=markup,
+        parse_mode="HTML",
     )
 
 
