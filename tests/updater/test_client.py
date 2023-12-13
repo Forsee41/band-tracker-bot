20787be06fea4420442812ba56125831f3c0e1e7
Forsee41 <forsee41@gmail.com>
2023-12-13 17:13:27 +0300

    add updater tests

---
+++ tests/updater/test_client.py
@@ -0,0 +1,197 @@
+from datetime import datetime
+
+import pytest
+
+from band_tracker.core.enums import EventSource
+from band_tracker.db.artist_update import ArtistUpdate, ArtistUpdateSocials
+from band_tracker.db.event_update import EventUpdate, EventUpdateSales
+from band_tracker.updater.deserializator import (
+    get_all_artists,
+    get_all_events,
+    get_artist,
+    get_event,
+)
+from test_data.temp_const import ARTISTS, ARTISTS_SMALL, EVENTS, EVENTS_SMALL
+
+
+class TestClient:
+    @pytest.mark.parametrize(
+        "raw_artist, expected_artist",
+        [
+            (
+                ARTISTS.get("_embedded").get("attractions")[0],
+                ArtistUpdate(
+                    name="The Orb",
+                    socials=ArtistUpdateSocials(
+                        spotify="https://open.spotify.com/artist"
+                        "/5HAtRoEPUvGSA7ziTGB1cF?autoplay=true",
+                        instagram="http://www.instagram.com/theorblive",
+                        youtube="https://www.youtube.com"
+                        "/channel/UCpoyFBLTLfbT2Z1D1AnlvLg",
+                    ),
+                    aliases=[],
+                    tickets_link="https://www.ticketmaster.com"
+                    "/the-orb-tickets/artist/806748",
+                    source_specific_data={
+                        EventSource.ticketmaster_api: {"id": "K8vZ9171RCf"}
+                    },
+                    genres=["Rock", "Pop"],
+                    main_image="https://s1.ticketm.net/dam/a/5a1/"
+                    "d9a78cdb-a7ce-4e40-861d-4ad1c6b355a1_"
+                    "264701_RETINA_PORTRAIT_3_2.jpg",
+                    thumbnail_image="https://s1.ticketm.net/dam/"
+                    "a/5a1/d9a78cdb-a7ce-4e40-861d-4ad1c"
+                    "6b355a1_264701_RECOMENDATION_16_9.jpg",
+                ),
+            ),
+            (
+                ARTISTS.get("_embedded").get("attractions")[1],
+                ArtistUpdate(
+                    name="Jeff Tain Watts",
+                    tickets_link="https://www.ticketmaster.com/"
+                    "jeff-tain-watts-tickets/artist/844673",
+                    socials=ArtistUpdateSocials(
+                        **{"spotify": None, "youtube": None, "instagram": None}
+                    ),
+                    source_specific_data={
+                        EventSource.ticketmaster_api: {"id": "K8vZ9171OI0"}
+                    },
+                    aliases=[],
+                    genres=["Jazz", "Jazz"],
+                    main_image="https://s1.ticketm.net/dam/c/bea/"
+                    "03d47f66-d37b-4aca-aa17-0135be64dbea_1"
+                    "05801_RETINA_PORTRAIT_3_2.jpg",
+                    thumbnail_image="https://s1.ticketm.net/dam/"
+                    "c/bea/03d47f66-d37b-4aca-"
+                    "aa17-0135be64dbea_105801_RECOMENDATIO"
+                    "N_16_9.jpg",
+                ),
+            ),
+        ],
+    )
+    def test_get_artist(self, raw_artist: dict, expected_artist: ArtistUpdate) -> None:
+        assert get_artist(raw_artist) == expected_artist
+
+    def test_get_event(self) -> None:
+        raw_event = EVENTS.get("_embedded").get("events")[0]
+        event = EventUpdate(
+            title="Shania Twain: Queen Of Me Tour",
+            date=datetime(2023, 7, 15),
+            venue="Ruoff Music Center",
+            ticket_url="https://concerts.livenation.com/shania-twain-queen-of-me-tour-"
+            "noblesville-indiana-07-15-2023/event/05005D55DD6D454E",
+            source_specific_data={
+                EventSource.ticketmaster_api: {"id": "vvG1fZ949qhf4C"}
+            },
+            venue_city="Noblesville",
+            venue_country="United States Of America",
+            artists=["K8vZ91719n0", "K8vZ917_bOf"],
+            image="https://s1.ticketm.net/dam/a/1d1/47cc9b10-4904-"
+            "4dec-b1d6-539e44a521d1_1825531_RETINA_PORTRAIT_3_2.jpg",
+            sales=EventUpdateSales(
+                sale_start=datetime(2022, 11, 4),
+                sale_end=datetime(2023, 7, 16),
+                price_max=249.95,
+                price_min=39.95,
+                currency="USD",
+            ),
+        )
+        assert get_event(raw_event) == event
+
+    def test_get_all_artists(self) -> None:
+        artists_list = [
+            ArtistUpdate(
+                name="Solina Cello-Ensemble",
+                socials=ArtistUpdateSocials(
+                    spotify=None,
+                    instagram=None,
+                    youtube=None,
+                ),
+                aliases=[],
+                tickets_link="https://www.ticketmaster.com/solina"
+                "-celloensemble-tickets/artist/2772347",
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "K8vZ917_Fi7"}
+                },
+                genres=["Classical", "Classical/Vocal"],
+                main_image="https://s1.ticketm.net/dam/c/518/83a05c63-479c-"
+                "4f7e-a7aa-1932aab77518_105461_RETINA_PORTRAIT_3_2.jpg",
+                thumbnail_image="https://s1.ticketm.net/dam/c/518/83a05c63-479c-4f7e"
+                "-a7aa-1932aab77518_105461_RECOMENDATION_16_9.jpg",
+            ),
+            ArtistUpdate(
+                name="Maze featuring Frankie Beverly",
+                socials=ArtistUpdateSocials(
+                    spotify=None,
+                    instagram=None,
+                    youtube=None,
+                ),
+                aliases=["frankie beverly and maze", "maze frankie beverly"],
+                tickets_link="https://www.ticketmaster.com/maze-f"
+                "eaturing-frankie-beverly-tickets/artist/735607",
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "K8vZ9171Idf"}
+                },
+                genres=["R&B", "Soul"],
+                main_image="https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b884-"
+                "4406b8ab64cd_1600911_RETINA_PORTRAIT_3_2.jpg",
+                thumbnail_image="https://s1.ticketm.net/dam/a/4cd/f4b129cc-7197-4ff0-b"
+                "884-4406b8ab64cd_1600911_RECOMENDATION_16_9.jpg",
+            ),
+        ]
+        assert get_all_artists(ARTISTS_SMALL) == artists_list
+
+    def test_get_all_events(self) -> None:
+        events_list = [
+            EventUpdate(
+                title="Shania Twain: Queen Of Me Tour",
+                date=datetime(2023, 7, 15),
+                venue="Ruoff Music Center",
+                ticket_url="https://concerts.livenation."
+                "com/shania-twain-queen-of-me-tour-"
+                "noblesville-indiana-07-15-2023/event/05005D55DD6D454E",
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "vvG1fZ949qhf4C"}
+                },
+                venue_city="Noblesville",
+                venue_country="United States Of America",
+                artists=["K8vZ91719n0", "K8vZ917_bOf"],
+                image="https://s1.ticketm.net/dam/a/1d1/47cc9b10-"
+                "4904-4dec-b1d6-539e44a521d1_1825531_RETINA_PORTRAIT_3_2.jpg",
+                sales=EventUpdateSales(
+                    sale_start=datetime(2022, 11, 4),
+                    sale_end=datetime(2023, 7, 16),
+                    price_max=249.95,
+                    price_min=39.95,
+                    currency="USD",
+                ),
+            ),
+            EventUpdate(
+                title="Imagine Dragons",
+                date=datetime(2023, 7, 8),
+                venue="American Family Insurance Amphitheater - Summerfest Grounds",
+                ticket_url="https://www.ticketmaster.com/imagine-dragon"
+                "s-milwaukee-wisconsin-07-08-20"
+                "23/event/07005D689BED1C00",
+                source_specific_data={
+                    EventSource.ticketmaster_api: {"id": "vvG1jZ9pxhDoYZ"}
+                },
+                venue_city="Milwaukee",
+                venue_country="United States Of America",
+                artists=["K8vZ917GSz7"],
+                image="https://s1.ticketm.net/dam/a/c90/67a18d21-394f"
+                "-4afe-84f9-15560d797c90_1652911_RETINA_PORTRAIT_3_2.jpg",
+                sales=EventUpdateSales(
+                    sale_start=datetime(2022, 11, 18),
+                    sale_end=datetime(2023, 7, 9),
+                    price_max=231.38,
+                    price_min=87.63,
+                    currency="USD",
+                ),
+            ),
+        ]
+        assert get_all_events(EVENTS_SMALL) == events_list
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ tests/updater/test_iterator.py
@@ -0,0 +1,190 @@
+import asyncio
+import json
+from datetime import datetime, timedelta
+from typing import Callable
+from unittest.mock import AsyncMock, patch
+
+import pytest
+
+from band_tracker.updater.errors import (
+    InvalidResponseStructureError,
+    InvalidTokenError,
+    RateLimitViolation,
+)
+from band_tracker.updater.page_iterator import ApiClient, PageIterator
+from band_tracker.updater.timestamp_predictor import LinearPredictor, TimestampPredictor
+
+
+@pytest.fixture()
+def mock_response() -> Callable[[str], dict]:
+    def lol(name: str = "invalid_structure_error") -> dict:
+        file_path = "tests/test_data/iterator_mock"
+        with open(f"{file_path}/{name}.json", "r") as f:
+            response_dict = json.load(f)
+
+        return response_dict
+
+    return lol
+
+
+@patch("band_tracker.updater.page_iterator.ApiClient.make_request")
+class TestIterator:
+    custom_request = ApiClient("", {})
+
+    @pytest.mark.slow
+    async def test_chunk_progression(
+        self,
+        mock_get: AsyncMock,
+        mock_response: Callable[[str], dict],
+        get_timestamp_predictor: Callable[[timedelta], TimestampPredictor],
+    ) -> None:
+        async def mock_make_request(
+            start_date: datetime,
+            end_date: datetime,
+            page_number: int = 0,
+            country_code: str = "",
+        ) -> dict:
+            await asyncio.sleep(0.1)
+            return mock_response(f"page{page_number + 1}")
+
+        mock_get.side_effect = mock_make_request
+
+        predictor = get_timestamp_predictor(timedelta(days=365))
+        iterator = PageIterator(self.custom_request, predictor=predictor)
+        initial_max_date = iterator.max_end_time
+
+        async for i in iterator:
+            pass
+
+        chunks = iterator.chunks
+
+        assert len(chunks) == 2
+        assert iterator.all_chunks_done() is True
+        assert iterator.max_end_time > initial_max_date
+
+    @pytest.mark.slow
+    @pytest.mark.skip
+    async def test_process_large_chunk(
+        self,
+        mock_get: AsyncMock,
+        mock_response: Callable[[str], dict],
+        get_timestamp_predictor: Callable[[timedelta], TimestampPredictor],
+    ) -> None:
+        async def mock_make_request(
+            start_date: datetime,
+            end_date: datetime,
+            page_number: int = 0,
+            country_code: str = "",
+        ) -> dict:
+            await asyncio.sleep(0.1)
+
+            if country_code == "US" or country_code == "BR" or country_code == "":
+                return mock_response("large_page")
+            else:
+                return mock_response("processed_page")
+
+        mock_get.side_effect = mock_make_request
+
+        predictor = get_timestamp_predictor(timedelta(days=0))
+        iterator = PageIterator(self.custom_request, predictor=predictor)
+        iterator.max_end_time += timedelta(days=731)
+
+        async for i in iterator:
+            pass
+
+        assert len(iterator.chunks) == 2
+
+    @pytest.mark.slow
+    async def test_structure_error(
+        self,
+        mock_get: AsyncMock,
+        mock_response: Callable[[str], dict],
+        get_linear_predictor: Callable[[float, float], LinearPredictor],
+    ) -> None:
+        async def mock_make_request(
+            start_date: datetime,
+            end_date: datetime,
+            page_number: int = 0,
+            country_code: str = "",
+        ) -> dict:
+            await asyncio.sleep(0.1)
+            if page_number == 3:
+                """simulation of response when invalid structure was given"""
+
+                return mock_response("invalid_structure_error")
+
+            return mock_response(f"page{page_number + 1}")
+
+        mock_get.side_effect = mock_make_request
+        predictor = get_linear_predictor(-0.1, 1000)
+
+        iterator = PageIterator(self.custom_request, predictor=predictor)
+
+        data = []
+        with pytest.raises(InvalidResponseStructureError):
+            async for i in iterator:
+                data.append(i)
+
+    @pytest.mark.slow
+    async def test_token_error(
+        self,
+        mock_get: AsyncMock,
+        mock_response: Callable[[str], dict],
+        get_linear_predictor: Callable[[float, float], LinearPredictor],
+    ) -> None:
+        async def mock_make_request(
+            start_date: datetime,
+            end_date: datetime,
+            page_number: int = 0,
+            country_code: str = "",
+        ) -> dict:
+            await asyncio.sleep(0.1)
+
+            if page_number == 3:
+                """simulation of response when invalid token was given"""
+
+                return mock_response("invalid_token")
+
+            return mock_response(f"page{page_number + 1}")
+
+        mock_get.side_effect = mock_make_request
+        predictor = get_linear_predictor(-0.1, 1000)
+
+        iterator = PageIterator(self.custom_request, predictor=predictor)
+
+        data = []
+        with pytest.raises(InvalidTokenError):
+            async for i in iterator:
+                data.append(i)
+
+    @pytest.mark.slow
+    async def test_rate_limit_error(
+        self,
+        mock_get: AsyncMock,
+        mock_response: Callable[[str], dict],
+        get_linear_predictor: Callable[[float, float], LinearPredictor],
+    ) -> None:
+        async def mock_make_request(
+            start_date: datetime,
+            end_date: datetime,
+            page_number: int = 0,
+            country_code: str = "",
+        ) -> dict:
+            await asyncio.sleep(0.1)
+
+            if page_number == 3:
+                """simulation of response when rate limit is exceeded"""
+
+                return mock_response("rate_limit_error")
+
+            return mock_response(f"page{page_number + 1}")
+
+        mock_get.side_effect = mock_make_request
+        predictor = get_linear_predictor(-0.1, 1000)
+
+        iterator = PageIterator(self.custom_request, predictor=predictor)
+
+        data = []
+        with pytest.raises(RateLimitViolation):
+            async for i in iterator:
+                data.append(i)
