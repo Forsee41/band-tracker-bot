1c073e841dfe83b88cc5def17115607500296ce9
Forsee41 <forsee41@gmail.com>
2023-12-15 02:02:22 +0300

    change test handler to use message manager

--- band_tracker/bot/handlers/test.py
+++ band_tracker/bot/handlers/test.py
@@ -3,14 +3,26 @@ import logging
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 
+from band_tracker.bot.helpers.get_user import get_user
+from band_tracker.bot.helpers.interfaces import MessageManager
+from band_tracker.core.enums import MessageType
+from band_tracker.db.dal_bot import BotDAL
+
 log = logging.getLogger(__name__)
 
 
 async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    dal: BotDAL = context.bot_data["dal"]
+    msg: MessageManager = context.bot_data["msg"]
+    assert update.effective_user
+    user = await get_user(dal=dal, tg_user=update.effective_user)
     if update.effective_chat:
         log.info("Test handler invoked")
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Test handler works!"
+        await msg.send_text(
+            text="Test handler works!",
+            markup=None,
+            user=user,
+            msg_type=MessageType.TEST,
         )
     else:
         log.warning("Test handler can't find an effective chat of an update")
