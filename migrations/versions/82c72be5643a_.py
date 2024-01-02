98d8c29fbbf6809584c35d2d93ce38ff41ef8573
Forsee <94901461+Forsee41@users.noreply.github.com>
2024-01-02 12:36:51 +0300

    Better env loading (#66)
    * environment changes
    
    * refactor checks
    
    * hide long make commands
    
    * mark slow tests
    
    * add revision make command
    
    * target specific containers in Makefile
    
    * remove direct make calls inside Makefile rules
    
    * remove phony in rabbit make rules

---
+++ migrations/versions/82c72be5643a_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 82c72be5643a
+Revises: 982e1aa38c99, f55dda35a584
+Create Date: 2023-12-26 07:38:56.379771
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "82c72be5643a"
+down_revision = ("982e1aa38c99", "f55dda35a584")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
