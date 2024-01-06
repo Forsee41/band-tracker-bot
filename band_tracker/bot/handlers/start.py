b4f1afb74c29c29aa609a9782ba665e33c3140bb
Forsee41 <forsee41@gmail.com>
2024-01-06 16:14:51 +0300

    change start handler to use custom context

--- band_tracker/bot/handlers/start.py
+++ band_tracker/bot/handlers/start.py
@@ -1,33 +1,19 @@
 import logging
 
 from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
+from telegram.ext import CommandHandler
 
-from band_tracker.bot.helpers.get_user import get_user
-from band_tracker.bot.helpers.interfaces import MessageManager
+from band_tracker.bot.helpers.context import BTContext
 from band_tracker.core.enums import MessageType
-from band_tracker.db.dal_bot import BotDAL
 
 log = logging.getLogger(__name__)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    dal: BotDAL = context.bot_data["dal"]
-    msg: MessageManager = context.bot_data["msg"]
-
-    user_tg = update.effective_user
-    if not user_tg:
-        log.warning("Start handler can't find an effective user!")
-        return
-
-    user = await get_user(tg_user=user_tg, dal=dal)
-
-    if not update.effective_chat:
-        log.warning("Start handler can't find an effective chat of an update")
-        return
+async def start(_: Update, ctx: BTContext) -> None:
+    user = await ctx.user()
 
     welcoming_text = f"Welcome {user.name}! Use `/help` command to get started!"
-    await msg.send_text(
+    await ctx.msg.send_text(
         text=welcoming_text,
         markup=None,
         user=user,
