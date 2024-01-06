916fd7ca4a444e287f3cc71ab7675844fa7a7fc6
Forsee41 <forsee41@gmail.com>
2024-01-06 16:13:00 +0300

    change query handler to use custom context

--- band_tracker/bot/handlers/query.py
+++ band_tracker/bot/handlers/query.py
@@ -1,30 +1,25 @@
 import logging
 
 from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
+from telegram.ext import CommandHandler
 
-from band_tracker.bot.helpers.get_user import get_user
-from band_tracker.bot.helpers.interfaces import MessageManager
+from band_tracker.bot.helpers.context import BTContext
 from band_tracker.core.enums import MessageType
-from band_tracker.db.dal_bot import BotDAL
 
 log = logging.getLogger(__name__)
 
 
-async def query_artists(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    dal: BotDAL = context.bot_data["dal"]
-    msg: MessageManager = context.bot_data["msg"]
-    assert update.effective_user
-    user = await get_user(tg_user=update.effective_user, dal=dal)
+async def query_artists(update: Update, ctx: BTContext) -> None:
+    user = await ctx.user()
 
-    args = context.args
+    args = ctx.args
     assert args
     query = "".join(args)
 
     if len(query) > 255:
         query = query[:255]
 
-    result_artists = await dal.search_artist(query)
+    result_artists = await ctx.dal.search_artist(query)
     result_artist_names = [artist.name for artist in result_artists]
 
     if result_artist_names:
@@ -33,7 +28,7 @@ async def query_artists(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         result_str = "No artists found!"
 
     if update.effective_chat:
-        await msg.send_text(
+        await ctx.msg.send_text(
             text=result_str,
             markup=None,
             user=user,
