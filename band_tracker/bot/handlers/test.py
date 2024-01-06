62e70606162d8e08db3facdf6776f194e2fd584e
Forsee41 <forsee41@gmail.com>
2024-01-06 16:16:30 +0300

    change test handler to use custom context

--- band_tracker/bot/handlers/test.py
+++ band_tracker/bot/handlers/test.py
@@ -1,31 +1,26 @@
 import logging
 
 from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
+from telegram.ext import CommandHandler
 
-from band_tracker.bot.helpers.get_user import get_user
-from band_tracker.bot.helpers.interfaces import MessageManager
+from band_tracker.bot.helpers.context import BTContext
 from band_tracker.core.enums import MessageType
-from band_tracker.db.dal_bot import BotDAL
 
 log = logging.getLogger(__name__)
 
 
-async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    dal: BotDAL = context.bot_data["dal"]
-    msg: MessageManager = context.bot_data["msg"]
-    assert update.effective_user
-    user = await get_user(dal=dal, tg_user=update.effective_user)
-    if update.effective_chat:
-        log.info("Test handler invoked")
-        await msg.send_text(
-            text="Test handler works!",
-            markup=None,
-            user=user,
-            msg_type=MessageType.TEST,
-        )
-    else:
+async def test(update: Update, ctx: BTContext) -> None:
+    user = await ctx.user()
+    if not update.effective_chat:
         log.warning("Test handler can't find an effective chat of an update")
+
+    log.info("Test handler invoked")
+    await ctx.msg.send_text(
+        text="Test handler works!",
+        markup=None,
+        user=user,
+        msg_type=MessageType.TEST,
+    )
 
 
 handlers = [CommandHandler("test", test)]
